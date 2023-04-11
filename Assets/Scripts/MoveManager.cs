d675a7509b3d3f47662fff72ed76043c2d9c9068
Spencer Peterson <89657089+Aspenar@users.noreply.github.com>
2023-04-10 18:22:32 -0600

    tried some new stuff for teleporting

--- Assets/Scripts/MoveManager.cs
+++ Assets/Scripts/MoveManager.cs
@@ -6,18 +6,33 @@ public class MoveManager : MonoBehaviour
 {
     public float moveTimer= 10.0f;
     public GameObject[] moveTargets;
-    public float timer;
-    public Vector3 nextMove;
-    public Vector3 currentSpot;
+    public GameObject Player;
+    //public Vector3 nextMove;
+    //public Vector3 currentSpot;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Player = GameObject.Find("Player");
+       
     }
+     void reset_moveTimer()
+        {
+            moveTimer += 10.0f;
+        }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(moveTimer > 0)
+        {
+            moveTimer -= Time.deltaTime;
+        } 
+        else if(moveTimer == 0) {
+
+            var i = Random.Range(0, moveTargets.Length);
+            Player.transform.position = moveTargets[i].position;
+            Player.transform.rotation = moveTargets[i].rotation;
+            reset_moveTimer();
+        }
     }
 }
