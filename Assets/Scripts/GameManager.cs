771a3477855dc4e3833c3ed7971a8c54ceb0d21b
Spencer Peterson <89657089+Aspenar@users.noreply.github.com>
2023-11-20 21:08:54 -0700

    adding UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private GameObject ScoreUI;
+    [SerializeField] private Text scoreText;
+    [SerializeField] private Text messageText;
     public int _score;
     // Start is called before the first frame update
     void Start()
     {
         _score = 0;
+        messageText.text = "press X to Spawn boxes, try not to blow anyone up.";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        scoreText.text = _score.ToString();
     }
 }
