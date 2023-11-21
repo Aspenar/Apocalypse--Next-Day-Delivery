b06149cfbc6cd2b13a4de5b63f9485ecdf9667ad
Spencer Peterson <89657089+Aspenar@users.noreply.github.com>
2023-11-20 22:37:06 -0700

    Added win Conditions
    Players can now win/lose based on score, restarting enabled as well. adjusted some materials.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,16 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private GameObject WinUI;
+    [SerializeField] private GameObject LoseUI;
     [SerializeField] private GameObject ScoreUI;
     [SerializeField] private Text scoreText;
     [SerializeField] private Text messageText;
     public int _score;
+    private bool gameOver;
+    private bool restart;
     // Start is called before the first frame update
     void Start()
     {
+        gameOver = false;
+        WinUI.SetActive(false);
+        LoseUI.SetActive(false);
         _score = 0;
         messageText.text = "press X to Spawn boxes, try not to blow anyone up.";
     }
@@ -27,6 +36,30 @@ public class GameManager : MonoBehaviour
         else if (_score < 0)
         {
             messageText.text = "Stop blowing people's houses up";
+        }
+        if (_score <= -600)
+        {
+            LoseUI.SetActive(true);
+            ScoreUI.SetActive(false);
+            gameOver = true;
+        }
+        if (_score >= 800)
+        {
+            WinUI.SetActive(true);
+            ScoreUI.SetActive(false);
+            gameOver = true;
+        }
+    }
+        public void onRestart(InputAction.CallbackContext context)
+    {
+        restart = context.action.triggered;
+        RestartButton();
+    }
+    public void RestartButton()
+    {
+        if (gameOver == true)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
 }
